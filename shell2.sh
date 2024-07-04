17b9bdaa4bef57b8282144818ff9c37854edf9d0
mangugopikrishna <mangu.gopikrishna37@gmail.com>
2024-07-04 20:17:56 +0530

    Update shell2.sh

--- shell2.sh
+++ shell2.sh
@@ -6,14 +6,14 @@ mkdir sample2_folder
 
 #creating files
 cd sample2_folder
-touch file_x file_y 
+touch file_x1 file_y1 
 cd ..
 
 #creating one more folder
 
 mkdir sample3_folder
 cd sample3_folder 
-touch file_z
+touch file_z1
 cd ..
 
 
